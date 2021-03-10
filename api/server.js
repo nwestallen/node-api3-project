90e2a584d080e60448a4efdfa643ff1c6bb8088e
Nick Allen <nwestallen@gmail.com>
2021-03-10 18:20:02 -0500

    initial server setup

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const server = express();
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
